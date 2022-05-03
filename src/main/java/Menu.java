5b7fc2678693be79ee2bafb12e8118b6f0d3fe88
Avihay Swissa <avihay126@gmail.com>
2022-05-03 18:12:33 +0300

    עדכון תמונת רקע הוצאה לקבועים קוסמטיקה כללית

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -8,30 +8,49 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Menu extends JPanel {
-    public static final int INITIAL_ARRAY_CAPACITY = 5, BUTTON_WIDTH = 150, BUTTON_HEIGHT = 60, FONT_SIZE = 20, BUTTON_Y = 100;
-    public static final String LA_LIGA = "512", PREMIER_LIG = "1383", FRENCH_LIG = "29242", BUNDESLIGA = "19796", SERIA_A = "4909", ONE_WEB = "https://www.one.co.il/";
+    public static final int DELAY_TIME = 4000,
+    WIDTH_CHOOSE_BUTTON = 150,
+    HEIGHT_CHOOSE_BUTTON = 60, FONT_SIZE = 20,
+            Y_CHOOSE_BUTTON = 300;
+    public static final String IMAGE_NAME = "01 THE TABLE DATA.png", ONE_WEB = "https://www.one.co.il/";
+    public static final int X_RETURN_BUTTON = 750;
+    public static final int Y_RETURN_BUTTON = 550;
+    public static final int WIDTH_RETURN_BUTTON = 100;
+    public static final int HEIGHT_RETURN_BUTTON = 65;
+    public static final int X_COMBO_BOX = 200;
+    public static final int Y_COMBO_BOX = 300;
+    public static final int WIDTH_COMBO_BOX = 50;
+    public static final int HEIGHT_COMBO_BOX = 60;
+    public static final int X_RESULT_LABEL = 200;
+    public static final int Y_RESULT_LABEL = 250;
+    public static final int WIDTH_RESULT_LABEL = 500;
+    public static final int HEIGHT_RESULT_LABEL = 200;
+    public static final int SIZE_FONT_LABEL = 40;
+    public static final int Y_RANDOM_BUTTON = 100;
+    public static final String KIND_OF_FONT = "Ariel";
+    public static final int WIDTH_RANDOM_BUTTON = 150;
+    public static final int HEIGHT_RANDOM_BUTTON = 60;
     private ImageIcon backGround;
-    private ArrayList<JButton> leaguesNameButtons = new ArrayList<>();
-    private JButton chooseLocation;
+    private ArrayList<JButton> leaguesNameButtons;
+    private ArrayList<Element> leaguesElement;
+    private ArrayList<String> leagueName;
     private JComboBox<Integer> ranking;
-    private String websiteUrl;
-    private String[] leagueName;
-    private String[] leagueSerial;
+    private JButton chooseRank;
     private JButton returnButton;
     private JLabel resultLabel;
+    private String websiteUrl;
 
 
     public Menu(int x, int y, int width, int height) {
-
-        this.leagueName = new String[]{"ליגה ספרדית", "ליגה אנגלית", "ליגה צרפתית", "ליגה גרמנית", "ליגת איטלקית"};
-        this.leagueSerial = new String[]{LA_LIGA, PREMIER_LIG, FRENCH_LIG, BUNDESLIGA, SERIA_A};
         this.websiteUrl = ONE_WEB;
+        leaguesNameButtons = new ArrayList<>();
+        initialLeague();
         this.setBounds(x, y, width, height);
         this.setLayout(null);
-        this.backGround = new ImageIcon("THE TABLE DATA.png");
-        for (int i = 0; i < this.leagueSerial.length; i++) {
-            JButton button = addButton(this.leagueName[i], this.getWidth() / 2 - BUTTON_WIDTH / 2, (i + 1) * BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT);
-            leaguesButtonListener(button, this.leagueSerial[i]);
+        this.backGround = new ImageIcon(IMAGE_NAME);
+        for (int i = 0; i < this.leagueName.size(); i++) {
+            JButton button = addButton(this.leagueName.get(i), this.getWidth() / 2 - WIDTH_RANDOM_BUTTON / 2, (i + 1) * Y_RANDOM_BUTTON, WIDTH_RANDOM_BUTTON, HEIGHT_RANDOM_BUTTON);
+            leaguesButtonListener(button, i);
             this.leaguesNameButtons.add(button);
 
         }
@@ -39,50 +58,61 @@ public class Menu extends JPanel {
         this.setVisible(true);
     }
 
+    private void initialLeague() {
+        try {
+            this.leaguesElement = getLeagues();
+            this.leagueName = getLeaguesName(this.leaguesElement);
+        } catch (IOException e) {
+            initialLeague();
+        }
 
-    private void leaguesButtonListener(JButton button, String leagueSerial) {
+    }
+
+
+    private void leaguesButtonListener(JButton button, int leagueIndex) {
         button.addActionListener((event) -> {
-            unVisibleButton();
-
-            comboBox(leagueSerial);
-            this.chooseLocation = addButton("Ranking", this.getWidth() / 2 - BUTTON_WIDTH / 2, 200, BUTTON_WIDTH, BUTTON_HEIGHT);
-            this.chooseLocation.addActionListener((event1) -> {
-                new Thread(()->{
+            try {
+                unVisibleButton();
+                comboBox(leagueIndex);
+                this.chooseRank = addButton("Ranking", this.getWidth() / 2 - WIDTH_CHOOSE_BUTTON / 2, Y_CHOOSE_BUTTON, WIDTH_CHOOSE_BUTTON, HEIGHT_CHOOSE_BUTTON);
+                chooseRankListener(leagueIndex);
+                this.returnButton = addButton("return", X_RETURN_BUTTON, Y_RETURN_BUTTON, WIDTH_RETURN_BUTTON, HEIGHT_RETURN_BUTTON);
+                repaint();
+                this.returnButton.addActionListener((event2) -> {
+                    restart();
                     deleteButton();
-                }).start();
-                new Thread(()->{
-                    try {
-                        progress(this.ranking.getSelectedIndex(), leagueSerial);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-
-
-            });
-            this.returnButton = addButton("return", 750, 550, 100, 65);
-            this.add(this.returnButton);
-            repaint();
-            this.returnButton.addActionListener((event2) -> {
-                restart();
-                deleteButton();
-            });
+                });
+            } catch (IOException e) {
+                System.out.println("l");
+                leaguesButtonListener(button, leagueIndex);
+            }
 
         });
     }
 
-    private void comboBox(String leagueSerial) {
-        int leagueSize = 0;
+    private void chooseRankListener(int leagueIndex) {
+        this.chooseRank.addActionListener((event1) -> {
+            new Thread(() -> {
+                deleteButton();
+            }).start();
+            new Thread(() -> {
+                try {
+                    progress(this.ranking.getSelectedIndex(), leagueIndex);
+                } catch (Exception e) {
+                    System.out.println("p");
+                    progress(this.ranking.getSelectedIndex(), leagueIndex);
+                }
+            }).start();
 
-        try {
-            leagueSize = getLeagueSize(leagueSerial);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        this.ranking = new JComboBox<Integer>(teamsInLeague(leagueSize));
-        this.ranking.setBounds(200, 200, 50, BUTTON_HEIGHT);
+
+        });
+    }
+
+    private void comboBox(int leagueIndex) throws IOException {
+        int leagueSize = 0;
+        leagueSize = getLeagueSize(leagueIndex);
+        this.ranking = new JComboBox<>(teamsInLeague(leagueSize));
+        this.ranking.setBounds(X_COMBO_BOX, Y_COMBO_BOX, WIDTH_COMBO_BOX, HEIGHT_COMBO_BOX);
         this.add(this.ranking);
 
     }
@@ -103,19 +133,58 @@ public class Menu extends JPanel {
         return button;
     }
 
+    private ArrayList<Element> getLeagues() throws IOException {
+        Document website = Jsoup.connect(this.websiteUrl).get();
+        ArrayList<Element> allLeagues = website.getElementsByClass("one-navigation-right-container");
+        Element leagues = allLeagues.get(0);
+        ArrayList<Element> relevantLeagues = new ArrayList<>();
+        int i = 0;
+        while (!leagues.child(i).text().equals("כדורגל עולמי")) {
+            i++;
+        }
+        while (!leagues.child(i).text().equals("ליגה בלגית")) {
+            if (ignore(leagues.child(i).text())) {
+                relevantLeagues.add(leagues.child(i));
+            }
+            i++;
+        }
+        return relevantLeagues;
 
-    private int getLeagueSize(String leagueSerial) throws IOException {
+    }
+
+    private boolean ignore(String leagueName) {
+        return (!leagueName.equals("חדשות") && !leagueName.equals("ליגת האלופות")
+                && !leagueName.equals("הליגה האירופית") && !leagueName.equals("קונפרנס ליג")
+                && !leagueName.equals("כדורגל עולמי")) && !leagueName.equals("");
+    }
+
+    private ArrayList<String> getLeaguesName(ArrayList<Element> leagues) throws IOException {
+        ArrayList<String> leaguesName = new ArrayList<>();
+        for (int i = 0; i < leagues.size(); i++) {
+            leaguesName.add(leagues.get(i).text());
+        }
+        return leaguesName;
+    }
+
+
+    private int getLeagueSize(int leagueIndex) throws IOException {
         int size = 0;
-        Element table = getTable(leagueSerial);
+        Element table = getTable(leagueIndex);
         size = table.childNodeSize();
         return size;
     }
 
-    private Document leagueSearch(String leagueSerial) throws IOException {
-        Document website = Jsoup.connect(this.websiteUrl).get();
-        ArrayList<Element> leagueList = website.getElementsByClass("one-navigation-right-item one-navigation-right-item-" + leagueSerial);
-        String leagueLink = leagueList.get(0).attr("href");
-        Document leagueUrl = Jsoup.connect(this.websiteUrl + leagueLink).get();
+    private Document getLeagueUrl(int leagueIndex) {
+        String leagueLink = this.leaguesElement.get(leagueIndex).attr("href");
+        Document leagueUrl = null;
+        try {
+            leagueUrl = Jsoup.connect(this.websiteUrl + leagueLink).get();
+
+        } catch (IOException e) {
+            System.out.println("URL");
+            leagueUrl = getLeagueUrl(leagueIndex);
+        }
+
         return leagueUrl;
     }
 
@@ -128,40 +197,44 @@ public class Menu extends JPanel {
 
     private Integer[] teamsInLeague(Integer numOfTeams) {
         Integer[] teams = new Integer[numOfTeams];
-        int location = 1;
+        int rank = 1;
         for (int i = 0; i < teams.length; i++) {
-            teams[i] = location;
-            location++;
+            teams[i] = rank;
+            rank++;
 
         }
         return teams;
     }
 
-    private void progress(int rank, String leagueSerial) throws IOException, InterruptedException {
-        Element table = getTable(leagueSerial);
+    private void progress(int rank, int leagueIndex) {
+        Element table = null;
+        table = getTable(leagueIndex);
         Element team = table.child(rank);
         String teamName = team.getElementsByClass("teamname").text();
         String teamPoints = team.getElementsByClass("points").text();
         System.out.println("  שם הקבוצה: " + teamName + " ניקוד: " + " " + teamPoints);
-        this.resultLabel =addLabel(teamName+" "+teamPoints+" " +"נקודות",200,90 ,600,200);
+        this.resultLabel = addLabel(teamName + " " + teamPoints + " " + "נקודות", X_RESULT_LABEL, Y_RESULT_LABEL, WIDTH_RESULT_LABEL, HEIGHT_RESULT_LABEL);
 
         repaint();
-        Thread.sleep(10000);
+        try {
+            Thread.sleep(DELAY_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         restart();
     }
 
-    private Element getTable(String leagueSerial) throws IOException {
-        Document leagueUrl = leagueSearch(leagueSerial);
+    private Element getTable(int leagueIndex) {
+        Document leagueUrl = getLeagueUrl(leagueIndex);
         Element table = tableSearch(leagueUrl);
         return table;
-
     }
 
     private void restart() {
         for (int i = 0; i < this.leaguesNameButtons.size(); i++) {
             this.leaguesNameButtons.get(i).setVisible(true);
         }
-        if (this.resultLabel !=null){
+        if (this.resultLabel != null) {
             this.resultLabel.setVisible(false);
         }
 
@@ -174,15 +247,15 @@ public class Menu extends JPanel {
     }
 
     private void deleteButton() {
-        this.chooseLocation.setVisible(false);
+        this.chooseRank.setVisible(false);
         this.ranking.setVisible(false);
         this.returnButton.setVisible(false);
     }
 
-    public  JLabel addLabel(String labelText, int x, int y, int width, int height) {
+    public JLabel addLabel(String labelText, int x, int y, int width, int height) {
         JLabel label = new JLabel(labelText);
         label.setForeground(Color.black);
-        Font font=new Font("Ariel",Font.BOLD,50);
+        Font font = new Font(KIND_OF_FONT, Font.BOLD, SIZE_FONT_LABEL);
         label.setFont(font);
         label.setBounds(x, y, width, height);
         this.add(label);
